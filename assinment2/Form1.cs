9daf3986f4b6c3cb98032691eca55d8bba473b6d
Jakub Wroblewski <RC0042145@student.riversidecollege.ac.uk>
2020-11-06 16:18:07 +0000

    added some comments

--- assinment2/Form1.cs
+++ assinment2/Form1.cs
@@ -40,18 +40,19 @@ namespace assinment2
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            try
+            try//This tries the code and if there is a mistake it catches later and diplays an error message stopping the code from crashing.
             {
-                string input = u.Text;
-                double height = double.Parse(h.Text);
-                double length = double.Parse(l.Text);
-                double width = double.Parse(w.Text);
-                double volume = height * length * width;
-                label4.Text = "Volume = " + volume + input + "^3";
+                string input = u.Text;//This takes the units inputted by the user into the text box 'u' as a string and creates a variable 'input'. 
+                double height = double.Parse(h.Text);//Here I am creating a variable 'height' from what the user inputs into the textbox 'h' and converts it into a number so that a calculation can be proformed using the user inputted value.
+                double length = double.Parse(l.Text);//Here I am creating a variable 'length' from what the user inputs into the textbox 'l' and converts it into a number so that a calculation can be proformed using the user inputted value.
+                double width = double.Parse(w.Text);//Here I am creating a variable 'width' from what the user inputs into the textbox 'w' and converts it into a number so that a calculation can be proformed using the user inputted value.
+                double volume = height * length * width;//Here I am creating a variable 'volume' which consists of the calculation for volume of a cuboid using the variables 'height', 'length', and 'width'. 
+                label4.Text = "Volume = " + volume + input + "^3";//Here I am editing the text in label4 so that it displays 'Volume =' and the variable 'volume' which will be the answer to the volume using the users variabels and the units also inputted by the user and '^3' to show that it is the units cubed to show it is volume.  
             }
-            catch
+            catch//This catches the error and displays an error message and stops the code from crashing. 
             {
-                MessageBox.Show("!ERROR! what did you think was going to happen?");
+                MessageBox.Show("!ERROR! what did you think was going to happen?");//This displays an error message saying '!ERROR! what did you think was going to happen?'.
+                label4.Text = "Answer = Sintax Error";//This changes the text in label 4 to 'Answer = Sintax Error'. 
             }
             
 
@@ -69,18 +70,19 @@ namespace assinment2
 
         private void button2_Click(object sender, EventArgs e)
         {
-            try
+            try//This tries the code and if there is a mistake it catches later and diplays an error message stopping the code from crashing.
             {
-                string input = u.Text;
-                double height = double.Parse(h.Text);
-                double length = double.Parse(l.Text);
-                double width = double.Parse(w.Text);
-                double sa = 2 * (length * height + length * width + width * height);
-                label4.Text = "Sufrace area = " + sa + input + "^2";
+                string input = u.Text;//This takes the units inputted by the user into the text box 'u' as a string and creates a variable 'input'.
+                double height = double.Parse(h.Text);//Here I am creating a variable 'height' from what the user inputs into the textbox 'h' and converts it into a number so that a calculation can be proformed using the user inputted value.
+                double length = double.Parse(l.Text);//Here I am creating a variable 'length' from what the user inputs into the textbox 'l' and converts it into a number so that a calculation can be proformed using the user inputted value.
+                double width = double.Parse(w.Text);//Here I am creating a variable 'width' from what the user inputs into the textbox 'w' and converts it into a number so that a calculation can be proformed using the user inputted value.
+                double sa = 2 * (length * height + length * width + width * height);//Here I am creating a variable 'sa'(Surface area) which consists of the calculation for surface area of a cuboid using the variables 'height', 'length', and 'width' and regular unchainging numbers.
+                label4.Text = "Sufrace area = " + sa + input + "^2";//Here I am editing the text in label4 so that it displays 'Surface area =' and the variable 'sa' which will be the answer to the surface area using the users variabels and the units also inputted by the user and '^2' to show that it is the units squared to show it is area.
             }
-            catch
+            catch//This catches the error and displays an error message and stops the code from crashing.
             {
-                MessageBox.Show("!ERROR! what did you think was going to happen?");
+                MessageBox.Show("!ERROR! what did you think was going to happen?");//This displays an error message saying '!ERROR! what did you think was going to happen?'.
+                label4.Text = "Answer = Sintax Error";//This changes the text in label 4 to 'Answer = Sintax Error'.
             }
         }
 
@@ -91,16 +93,17 @@ namespace assinment2
 
         private void button3_Click(object sender, EventArgs e)
         {
-            try
+            try//This tries the code and if there is a mistake it catches later and diplays an error message stopping the code from crashing.
             {
-                string unit = u2.Text;
-                double radius = double.Parse(r.Text);
-                double S_volume = 4 * 3.14159 * radius * radius * radius / 3;
-                label9.Text = "Volume = " + S_volume + unit + "^3";
+                string unit = u2.Text;//This takes the units inputted by the user into the text box 'u2' as a string and creates a variable 'unit'.
+                double radius = double.Parse(r.Text);//Here I am creating a variable 'radius' from what the user inputs into the textbox 'r' and converts it into a number so that a calculation can be proformed using the user inputted values.
+                double S_volume = 4 * 3.14159 * radius * radius * radius / 3;//Here I am creating a variable 'S_volume' (sphere volume) which consists of the calculation for volume of a sphere using the variable 'radius' and regular unchainging numbers.
+                label9.Text = "Volume = " + S_volume + unit + "^3";//Here I am editing the text in label9 so that it displays 'Volume =' and the variable 'S_volume' which will be the answer to the volume using the users variable and the units also inputted by the user and '^3' to show that it is the units cubed to show it is volume.
             }
-            catch
+            catch//This catches the error and displays an error message and stops the code from crashing.
             {
-                MessageBox.Show("!ERROR! what did you think was going to happen?");
+                MessageBox.Show("!ERROR! what did you think was going to happen?");//This displays an error message saying '!ERROR! what did you think was going to happen?'.
+                label9.Text = "Answer = Sintx Error";//This changes the text in label 4 to 'Answer = Sintax Error'.
             }
         }
 
@@ -111,12 +114,12 @@ namespace assinment2
 
         private void button4_Click(object sender, EventArgs e)
         {
-            try
+            try// This tries the code and if there is a mistake it catches later and diplays an error message stopping the code from crashing.
             {
-                string unit = u2.Text;
-                double radius = double.Parse(r.Text);
-                double S_sa = 4 * 3.14159 * radius * radius;
-                label9.Text = "Surface area = " + S_sa + unit + "^2";
+                string unit = u2.Text;//This takes the units inputted by the user into the text box 'u2' as a string and creates a variable 'unit'.
+                double radius = double.Parse(r.Text);//Here I am creating a variable 'radius' from what the user inputs into the textbox 'r' and converts it into a number so that a calculation can be proformed using the user inputted values.
+                double S_sa = 4 * 3.14159 * radius * radius;//Here I am creating a variable 'S_sa' (sphere surface area) which consists of the calculation for the surface area of a sphere using the variable 'radius' and regular unchainging numbers.
+                label9.Text = "Surface area = " + S_sa + unit + "^2";//Here I am editing the text in label9 so that it displays 'Volume =' and the variable 'volume' which will be the answer to the volume using the users variabel and the units also inputted by the user and '^2' to show that it is the units squared to show it is area.
             }
             catch
             {
